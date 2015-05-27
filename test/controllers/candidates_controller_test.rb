f96660fff118e240705aa10315724d7591533884
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-27 15:17:00 -0400

    Rails generated controller setup for candidates

---
+++ test/controllers/candidates_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class CandidatesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    Candidate.new(race_id: 1, name: "Mr.Rothschild",
+    hometown_at: "London", district_at: "Entire World", party: "NWO")
+    get :show 
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+end
